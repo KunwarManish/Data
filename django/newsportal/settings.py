09c4dcfb1a6034682773d37bb106589c51419c33
Manish Kunwar <31987649+KunwarManish@users.noreply.github.com>
2022-07-05 21:02:40 +0545

    Update settings.py

--- django/newsportal/settings.py
+++ django/newsportal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m+e8q_9fdzedb8_)(v7m5dxj$o*pr#usiq$$bvi(n4bwpdjq+o'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
